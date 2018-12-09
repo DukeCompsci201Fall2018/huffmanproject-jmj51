82b3eba32472167b7c37ba92f62531bd9701b5a4
Michael Jiron <jmj51@duke.edu>
2018-12-08 21:13:38 -0500

    v3.0 Fix writeCompressedBits

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -128,11 +128,12 @@ public class HuffProcessor {
 
 	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out){
 
-		in.readBits(BITS_PER_INT);
-		for(String code: codings){
-			if(code.equals("") || code.equals(codings[PSEUDO_EOF])){
-				break;
-			}
+		while(true){
+			int value = in.readBits(BITS_PER_WORD);
+			if(value == -1) break;
+
+			String code = codings[value];
+
 			out.writeBits(code.length(), Integer.parseInt(code, 2));
 		}
 
